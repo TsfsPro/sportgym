3d27908824ef214420bef37c545df8b6601ef109
Valeriy <74008568+TsfsPro@users.noreply.github.com>
2023-01-09 23:21:04 +0300

    Имитация падения шарика
    Урок из книги.

---
+++ bals.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+#include "constans.h"
+
+//Функция вычисления высоты башни
+double visiHlen(double timeUse) {
+    double result = myConstans::my_gravity * (timeUse * timeUse) / 2;
+    return result;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите высоту башни: " << endl;
+    double heightTower = 0;
+    cin >> heightTower;
+    for (int i = 0; i <= 5; i++) {
+        double vremTime = visiHlen(i);
+        double heiBals = heightTower - vremTime;
+        if (heiBals > 0) {
+            cout << "\tСекунда:  " << i << "  высота мячика  " << heiBals << "  метров. " << endl;
+        }
+        else {
+            cout << "\tМяч достиг земли" << endl;
+        }
+    }
+
+}
---
+++ constans.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANS_H
+#define CONSTANS_H
+namespace myConstans
+{
+	const double my_gravity = 9.8;
+}
+
+#endif // !CONSTANS_H
+
